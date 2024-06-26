c6ae53d979c3e6ccd8210b64038ea869987b998c
itsmesunky <itisp_06@naver.com>
2024-06-26 17:58:10 +0900

    💄Design: 커뮤니티, 마이페이지지 화면 UI 생성

---
+++ app/screens/board/ManagePost.js
@@ -0,0 +1,9 @@
+/**
+ * 게시글 작성, 수정 화면
+ */
+
+function ManagePost() {
+  return;
+}
+
+export default ManagePost;
---
+++ app/screens/board/PostList.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+/**
+ * 게시글 리스트
+ */
+function PostList() {
+  return <View></View>;
+}
+
+export default PostList;
