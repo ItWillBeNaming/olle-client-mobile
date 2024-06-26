951858b99a0e86fe4506d9c17a552fa29698f2fb
itsmesunky <itisp_06@naver.com>
2024-06-26 23:34:35 +0900

    🚀Feat: 임시 로그인 API 연결

--- app/util/member/Auth.js
+++ app/util/member/Auth.js
@@ -4,11 +4,11 @@ export async function authenticate(mode, email, password) {
   const url = `http://43.201.194.56/api/v1/auth/${mode}`;
 
   const response = await axios.post(url, {
-    title: "test",
-    content: "test",
+    email: email,
+    password: password,
   });
 
-  return response.data;
+  return response.data.data.accessToken;
 }
 
 export async function login(email, password) {
